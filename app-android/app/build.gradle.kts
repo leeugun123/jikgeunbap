197654e373d8b38bdfa7d5be3e10e7623e338a75
leeugun123 <leeugun123@naver.com>
2025-11-17 23:03:21 +0900

    chore : Android Studio version upgrade 및 agp 버전 변경(8.6.0)

--- app-android/app/build.gradle.kts
+++ app-android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.jikgeunbap.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.jikgeunbap.app"
         minSdk = 34
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
