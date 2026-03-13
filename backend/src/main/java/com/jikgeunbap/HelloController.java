24c9fed1ec7efd83f31e9c150554a0733c9772ee
leeugun123 <leeugun123@naver.com>
2026-03-13 21:14:21 +0900

    fix : package 명 수정

--- backend/src/main/java/com/jikgeunbap/HelloController.java
+++ backend/src/main/java/com/jikgeunbap/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.jikgeunbap;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "스프링 부트 프로젝트가 성공적으로 열렸습니다!";
+        return "스프링 부트 프로젝트가 성공적으로 fewfwfwfe다!";
     }
 }
