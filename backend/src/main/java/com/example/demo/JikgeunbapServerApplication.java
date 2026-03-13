db4f94956137b24fa1c5f07b323d44d8a0dc0dc8
leeugun123 <leeugun123@naver.com>
2026-03-13 21:05:58 +0900

    chore : 초기 개발 환경 세팅

---
+++ backend/src/main/java/com/example/demo/JikgeunbapServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikgeunbapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JikgeunbapServerApplication.class, args);
+	}
+
+}
