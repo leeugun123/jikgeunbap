6809ce209cb7900e88a49034731e90f016a2339e
leeugun123 <leeugun123@naver.com>
2026-03-16 22:52:09 +0900

    fix : Restaurant domain import 추가

--- app-android/data/src/main/java/com/example/jikgeunbap/data/source/RemoteRestaurantDataSource.kt
+++ app-android/data/src/main/java/com/example/jikgeunbap/data/source/RemoteRestaurantDataSource.kt
@@ -1,5 +1,7 @@
 package com.example.jikgeunbap.data.source
 
+import com.example.jikgeunbap.domain.domain.model.Restaurant
+
 interface RemoteRestaurantDataSource {
-    suspend fun fetchRestaurants(): List<com.example.jikgeunbap.domain.domain.model.Restaurant>
+    suspend fun fetchRestaurants(): List<Restaurant>
 }
